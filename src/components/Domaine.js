974087444981ebd87866433e08ef2d31dcebef7c
miebi <iyeyemim@gmail.com>
2022-11-18 11:42:20 +0100

    WIP page domaine

--- src/components/Domaine.js
+++ src/components/Domaine.js
@@ -18,27 +18,26 @@ const Domaine = () => {
       </div>
 
       <div className="container-full bg-green">
-        <div className="container-child">
-          
-          <div className="bg-blur lg:w-7/12 md:w-full text-white -z-10 px-16 py-14">
+        <div className="container-child block md:flex space-x-4">
+          <div className="bg-blur md:w-5/12 flex-none sm:w-full text-white px-16 py-14 z-10 mb-8">
             <div>
               <h3><Trans i18nKey="domaine.h3-01">Situé en pleine forêt dans un cadre calme et reposant, AVALON est l’endroit idéal pour se déconnecter du quotidien et profiter de la nature.</Trans></h3>
-              <div className="flex justify-center space-x-10 mb-5">
-                <div className="inline-flex flex-col items-center w-3/12">
+              <div className="flex justify-center space-x-10 mb-5 mt-8">
+                <div className="inline-flex flex-col items-center text-center w-3/12">
                   <img src="images/domaine/picto-sanitaires.svg" class="picto-domaine" alt="Icone sanitaires" />
                   <h3><Trans i18nKey="domaine.sanitaires">Sanitaires</Trans></h3>
                 </div>
-                <div className="inline-flex flex-col items-center w-3/12">
+                <div className="inline-flex flex-col items-center text-center w-3/12">
                   <img src="images/domaine/picto-clubhouse.svg" class="picto-domaine" alt="Icone clubhouse" />
                   <h3><Trans i18nKey="domaine.clubhouse">Clubhouse</Trans></h3>
                 </div>
               </div>
-              <div className="flex justify-center space-x-10">
-                <div className="inline-flex flex-col items-center w-3/12">
+              <div className="flex justify-center space-x-10 mb-8">
+                <div className="inline-flex flex-col items-center text-center w-3/12">
                   <img src="images/domaine/picto-recharge.svg" class="picto-domaine" alt="Icone recharge" />
                   <h3><Trans i18nKey="domaine.recharge">Recharge</Trans></h3>
                 </div>
-                <div className="inline-flex flex-col items-center w-3/12">
+                <div className="inline-flex flex-col items-center text-center w-3/12">
                   <img src="images/domaine/picto-wifi.svg" class="picto-domaine" alt="Icone wifi" />
                   <h3><Trans i18nKey="domaine.wifi">Wifi</Trans></h3>
                 </div>
@@ -47,71 +46,85 @@ const Domaine = () => {
             </div>
           </div>
 
-          <h1 className="white text-right"><Trans i18nKey="domaine.h1-02">Des <strong>infrastructures</strong> pensées pour <strong>optimiser votre expérience.</strong></Trans></h1>
-          <img src="images/domaine/img-domaine-01.png" class="w-7/12" alt="Icone clubhouse" />
+          <div className=''>
+            <h1 className="white text-left md:text-right mb-10"><Trans i18nKey="domaine.h1-02">Des <strong>infrastructures</strong> pensées pour <strong>optimiser votre expérience.</strong></Trans></h1>
+            <img src="images/domaine/img-domaine-01.png" class="md:-translate-x-24" alt="Icone clubhouse" />
+          </div>
         </div>
       </div>
 
       <div className="container-full bg-mauve">
-        <div className="container-child">
-          <h1 className="white"><Trans i18nKey="domaine.h1-03">location de matériel, achat d’appâts... <strong>ON A PENSÉ À TOUT !</strong></Trans></h1>
-          <img src="images/domaine/img-domaine-02.png" class="w-3/4" alt="Icone clubhouse" />
-
-          <div className="bg-blur w-7/12 text-white -z-10 px-16 py-14">
-            <div>
-              <h3><Trans i18nKey="domaine.h3-02">Que ce soit en famille ou entre amis, AVALON CARP LAKE saura vous surprendre agréablement, tant au niveau de la qualité de vie qu’au niveau de la pêche.</Trans></h3>
-              <div className="flex justify-center space-x-6 mb-5">
-                <div className="inline-flex flex-col items-center w-4/12">
-                  <img src="images/domaine/picto-materiel.svg" class="picto-domaine" alt="Icone materiel" />
-                  <h3 className="text-center"><Trans i18nKey="domaine.location-materiel">Location de matériel</Trans></h3>
-                </div>
-                <div className="inline-flex flex-col items-center w-4/12">
-                  <img src="images/domaine/picto-appats.svg" class="picto-domaine" alt="Icone appats" />
-                  <h3 className="text-center"><Trans i18nKey="domaine.achat-appats">Achat<br/>d’appâts</Trans></h3>
-                </div>
-                <div className="inline-flex flex-col items-center w-4/12">
-                  <img src="images/domaine/picto-cabane.svg" class="picto-domaine" alt="Icone cabane" />
-                  <h3 className="text-center"><Trans i18nKey="domaine.cabanes">Cabanes de pêche sur chaque poste</Trans></h3>
+        <div className="container-child block md:flex md:space-x-4">
+          <div className='z-10 w-full md:w-1/2 flex-none'>
+            <h1 className="white mb-8"><Trans i18nKey="domaine.h1-03">location de matériel, achat d’appâts... <strong>ON A PENSÉ À TOUT !</strong></Trans></h1>
+            <div className="bg-blur text-white -z-10 px-16 py-14 mb-5">
+              <div>
+                <h3 className='mb-8'><Trans i18nKey="domaine.h3-02">Que ce soit en famille ou entre amis, AVALON CARP LAKE saura vous surprendre agréablement, tant au niveau de la qualité de vie qu’au niveau de la pêche.</Trans></h3>
+                <div className="flex justify-center space-x-6 mb-5">
+                  <div className="inline-flex flex-col items-center w-4/12">
+                    <img src="images/domaine/picto-materiel.svg" class="picto-domaine" alt="Icone materiel" />
+                    <h3 className="text-center font-normal"><Trans i18nKey="domaine.location-materiel">Location de matériel</Trans></h3>
+                  </div>
+                  <div className="inline-flex flex-col items-center w-4/12">
+                    <img src="images/domaine/picto-appats.svg" class="picto-domaine" alt="Icone appats" />
+                    <h3 className="text-center font-normal"><Trans i18nKey="domaine.achat-appats">Achat<br/>d’appâts</Trans></h3>
+                  </div>
+                  <div className="inline-flex flex-col items-center w-4/12">
+                    <img src="images/domaine/picto-cabane.svg" class="picto-domaine" alt="Icone cabane" />
+                    <h3 className="text-center font-normal"><Trans i18nKey="domaine.cabanes">Cabanes de pêche sur chaque poste</Trans></h3>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <p className="white"><Trans i18nKey="domaine.p-02">Avec des profondeurs allant de 1 à 6m ce plan d’eau mystérieux vous réserve de belles surprises. Ici vous pourrez louer : cannes, épuisette, rod-pod, bed chair, level chair etc. Des appâts (bouillettes & pellets) sont également disponibles afin que vous puissiez profiter un maximum de votre séjour.</Trans></p>
+          <div>
+            <img src="images/domaine/img-domaine-02.png" className="origin-top-right md:scale-125 w-screen md:w-full max-w-none md:max-w-full -translate-x-[32px]" alt="" />
+            <div className="md:grid grid-cols-6">
+              <div className='col-span-1'></div>
+              <p className="white md:translate-y-32 block md:col-span-4 mt-[3rem] md:mt-auto">
+                <Trans i18nKey="domaine.p-02">Avec des profondeurs allant de 1 à 6m ce plan d’eau mystérieux vous réserve de belles surprises. Ici vous pourrez louer : cannes, épuisette, rod-pod, bed chair, level chair etc. Des appâts (bouillettes & pellets) sont également disponibles afin que vous puissiez profiter un maximum de votre séjour.</Trans>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
     
       <div className="container-full bg-green">
-        <div className="flex container-child">
-          <div>
-            <div className="">
-              <img src="images/domaine/img-domaine-03.png" class="w-full" alt="Icone clubhouse" />
+        <div className="container-child block md:flex md:space-x-4">
+          {/* IMAGES */}
+          <h1 className="white text-left md:text-right w-full md:w-8/12 mb-10 block md:hidden"><Trans i18nKey="domaine.h1-04">plusieurs centaines de poissons vous attendent !</Trans></h1>
+
+          <div className='w-full md:w-1/2 flex-none origin-top-left md:scale-[115%] mb-10'>
+            <div className="mb-4">
+              <img src="images/domaine/img-domaine-03.png" class="w-full" alt="" />
             </div>
-            <div className="flex space-x-2">
-              <img src="images/domaine/img-domaine-04.png" class="w-5/12" alt="Icone clubhouse" />
-              <img src="images/domaine/img-domaine-05.png" class="w-7/12" alt="Icone clubhouse" />
+            <div className="grid grid-cols-2 md:grid-cols-7 gap-4 mb-10">
+              <img src="images/domaine/img-domaine-04.png" class="md:col-span-3" alt="" />
+              <img src="images/domaine/img-domaine-05.png" class="md:col-span-4" alt="" />
             </div>
           </div>
 
+          {/* TEXTE */}
           <div className="flex items-end flex-col">
-            <h1 className="white text-right w-7/12"><Trans i18nKey="domaine.h1-04">plusieurs centaines de poissons vous attendent !</Trans></h1>
+            <h1 className="white text-left md:text-right w-8/12 mb-10 hidden md:block"><Trans i18nKey="domaine.h1-04">plusieurs centaines de poissons vous attendent !</Trans></h1>
 
             <div className="bg-blur text-white px-16 py-14">
               <div>
                 <div className="flex justify-end">
-                  <h3 className="bold text-right w-8/12"><Trans i18nKey="domaine.h3-07">Carpes miroir & communes hors normes et silures très combatifs grâce à une eau pure de très grande qualité et de grande profondeur.</Trans></h3>
+                  <h3 className="bold text-right w-8/12 mb-8"><Trans i18nKey="domaine.h3-07">Carpes miroir & communes hors normes et silures très combatifs grâce à une eau pure de très grande qualité et de grande profondeur.</Trans></h3>
                 </div>
-                <div className="flex justify-center space-x-6 mb-5">
-                  <div className="inline-flex flex-col items-center w-3/12">
+                <div className="grid grid-cols-2 sm:grid-cols-3 justify-between gap-6 mb-5 mx-auto">
+                  <div className="inline-flex flex-col items-center">
                     <img src="images/domaine/picto-carp.svg" class="picto-domaine" alt="Icone carp" />
                     <h3 className="text-center"><Trans i18nKey="domaine.carp">Carp</Trans></h3>
                   </div>
-                  <div className="inline-flex flex-col items-center w-3/12">
+                  <div className="inline-flex flex-col items-center">
                     <img src="images/domaine/picto-catfish.svg" class="picto-domaine" alt="Icone catfish" />
                     <h3 className="text-center"><Trans i18nKey="domaine.catfish">Catfish</Trans></h3>
                   </div>
-                  <div className="inline-flex flex-col items-center w-3/12">
+                  <div className="inline-flex flex-col items-center col-span-2 sm:col-span-1">
                     <img src="images/domaine/picto-haute-densite.svg" class="picto-domaine" alt="Icone haute densite" />
                     <h3 className="text-center"><Trans i18nKey="domaine.haute-densite">Haute densité de poissons</Trans></h3>
                   </div>
