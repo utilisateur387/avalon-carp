236311590eaf030f68c818304685f9e29064e249
miebi <iyeyemim@gmail.com>
2022-11-19 22:51:51 +0100

    contact form + page galerie

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,18 +1,24 @@
 import {Link} from "react-router-dom";
 import { useTranslation, Trans } from 'react-i18next';
 import emailjs from 'emailjs-com';
+import { useState } from "react";
 
 const ContactForm = ({ title = true, infosPratiques = true }) => {
   const { t, i18n } = useTranslation();
+  const [formResult, setFormResult] = useState(null)
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_d692m41', 'template_yhb9gdf', e.target, 'nnuzeqvSI4LXDh3Uw')
+    emailjs.sendForm('service_onom4fm', 'template_yhb9gdf', e.target, 'nnuzeqvSI4LXDh3Uw') // SendGrid
+    // emailjs.sendForm('service_d692m41', 'template_yhb9gdf', e.target, 'nnuzeqvSI4LXDh3Uw') // Gmail
       .then((result) => {
           console.log(result.text);
-      }, (error) => {
+          setFormResult('success')
+          console.log(formResult);
+        }, (error) => {
           console.log(error.text);
+          setFormResult('error')
       });
 
       e.target.reset();
@@ -24,6 +30,7 @@ const ContactForm = ({ title = true, infosPratiques = true }) => {
             {title && 
               <h2 class="text-center mx-auto"><Trans i18nKey="form.preparez-maintenant">PRÉPAREZ DÈS MAINTENANT VOTRE <strong>PROCHAINE SESSION DE PÊCHE.</strong></Trans></h2>
             }
+
           <form onSubmit={sendEmail} action="" className="mx-auto contact-form">
             <div class="grid md:grid-cols-2 sm:grid-cols-1 md:gap-4">
               <input type="text" placeholder={t('form.nom')} name='lastName' />
@@ -38,10 +45,25 @@ const ContactForm = ({ title = true, infosPratiques = true }) => {
             <textarea type="textarea" placeholder={t('form.message')} className="w-full input-message" name='message'/>
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-              { !infosPratiques &&
+              { !infosPratiques && !formResult &&
                 <div></div>
               }
+
+              { (formResult == 'success') &&
+                <div className="form-message col-span-2">
+                  <Trans i18nKey="form.message-success">Votre message a bien été envoyé !</Trans>
+               </div>
+              }
+              { (formResult == 'error') &&
+                <div className="form-message col-span-2">
+                  <Trans i18nKey="form.message-error">Une erreur est survenue. Veuillez-nous contacter à avaloncarplake@gmail.com</Trans>
+                </div>
+              }
+
+              {/* ENVOYER */}
               <input type="submit" value={t('form.submit')} class="cta cta-green block"/>
+
+              {/* INFOS PRATIQUES */}
               { infosPratiques &&
                 <Link to="/contact" className="block">
                     <button class="cta cta-red w-full"><Trans i18nKey="form.infos-pratiques">Infos pratiques</Trans></button>
--- src/components/Galerie.js
+++ src/components/Galerie.js
@@ -5,9 +5,54 @@ const Galerie = () => {
   const { t, i18n } = useTranslation();
   
   return (
-    <div>
-      <h1 className='text-center green'><Trans i18nKey="galerie.h1">DÉCOUVREZ LES PLUS BELLES PHOTOS DE AVALON CARP LAKE.</Trans></h1>
-      
+    <div className='page-galerie'>
+      <div className="container-child">
+        <h1 className='text-center green mb-10'><Trans i18nKey="galerie.h1">DÉCOUVREZ LES PLUS BELLES PHOTOS DE AVALON CARP LAKE.</Trans></h1>
+          
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
+          <img src="images/galerie/img-galerie-46.jpg" alt="" />
+          <img src="images/galerie/img-galerie-47.jpg" alt="" />
+          <img src="images/galerie/img-galerie-48.jpg" alt="" />
+          <img src="images/galerie/img-galerie-49.jpg" alt="" />
+          <img src="images/galerie/img-galerie-50.jpg" alt="" />
+          <img src="images/galerie/img-galerie-51.jpg" alt="" />
+          <img src="images/galerie/img-galerie-52.jpg" alt="" />
+          <img src="images/galerie/img-galerie-53.jpg" alt="" />
+          <img src="images/galerie/img-galerie-54.jpg" alt="" />
+          <img src="images/galerie/img-galerie-55.jpg" alt="" />
+          <img src="images/galerie/img-galerie-56.jpg" alt="" />
+          <img src="images/galerie/img-galerie-57.jpg" alt="" />
+          <img src="images/galerie/img-galerie-58.jpg" alt="" />
+          <img src="images/galerie/img-galerie-59.jpg" alt="" />
+          <img src="images/galerie/img-galerie-60.jpg" alt="" />
+          <img src="images/galerie/img-galerie-61.jpg" alt="" />
+          <img src="images/galerie/img-galerie-62.jpg" alt="" />
+          <img src="images/galerie/img-galerie-63.jpg" alt="" />
+          <img src="images/galerie/img-galerie-64.jpg" alt="" />
+          <img src="images/galerie/img-galerie-65.jpg" alt="" />
+          <img src="images/galerie/img-galerie-66.jpg" alt="" />
+          <img src="images/galerie/img-galerie-67.jpg" alt="" />
+          <img src="images/galerie/img-galerie-68.jpg" alt="" />
+          <img src="images/galerie/img-galerie-69.jpg" alt="" />
+          <img src="images/galerie/img-galerie-70.jpg" alt="" />
+          <img src="images/galerie/img-galerie-71.jpg" alt="" />
+          <img src="images/galerie/img-galerie-72.jpg" alt="" />
+          <img src="images/galerie/img-galerie-73.jpg" alt="" />
+          <img src="images/galerie/img-galerie-74.jpg" alt="" />
+          <img src="images/galerie/img-galerie-75.jpg" alt="" />
+          <img src="images/galerie/img-galerie-76.jpg" alt="" />
+          <img src="images/galerie/img-galerie-77.jpg" alt="" />
+          <img src="images/galerie/img-galerie-78.jpg" alt="" />
+          <img src="images/galerie/img-galerie-79.jpg" alt="" />
+          <img src="images/galerie/img-galerie-80.jpg" alt="" />
+          <img src="images/galerie/img-galerie-81.jpg" alt="" />
+          <img src="images/galerie/img-galerie-82.jpg" alt="" />
+          <img src="images/galerie/img-galerie-83.jpg" alt="" />
+          <img src="images/galerie/img-galerie-84.jpg" alt="" />
+          <img src="images/galerie/img-galerie-85.jpg" alt="" />
+        </div>
+      </div>
+
       <div className="container-full bg-beige">
         <div className="container-child">
           <ContactForm />
